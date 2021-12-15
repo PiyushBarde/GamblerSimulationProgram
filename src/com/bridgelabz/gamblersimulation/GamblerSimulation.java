36580d49c3147e08ec248c10c49989b7ab794541
PiyushBarde <piyushbarde1@gmail.com>
2021-12-15 19:49:18 +0530

    ADDED: initial amount & bet

---
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.gamblersimulation;
+
+public class GamblerSimulation {
+
+	public static void main(String[] args) {
+		int currentAmount = 100;
+		System.out.println("Gambler starts every day with " + currentAmount + "$");
+		int betPlaced = 1;
+		System.out.println("and Playes the bet of " + betPlaced + "$");
+	}
+}
