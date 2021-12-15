19a4f0bb0f06857c4ff18db350244b1caea874b7
PiyushBarde <piyushbarde1@gmail.com>
2021-12-15 19:54:14 +0530

    ADDED: win or lose condition

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -7,5 +7,17 @@ public class GamblerSimulation {
 		System.out.println("Gambler starts every day with " + currentAmount + "$");
 		int betPlaced = 1;
 		System.out.println("and Playes the bet of " + betPlaced + "$");
+		int condition = (int) (Math.floor(Math.random()*10)%2);
+		if(condition==1)
+		{
+			currentAmount++;
+			System.out.println("Gambler wont the bet and he have " + currentAmount + " $");
+			
+		}
+		else
+		{
+			currentAmount--;
+			System.out.println("Gambler lost the bet and he have " + currentAmount + " $");	
+		}
 	}
 }
