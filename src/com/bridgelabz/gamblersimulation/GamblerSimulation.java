e38dc01f29e9ed89cb6b5f8532caef02ab0f5418
PiyushBarde <piyushbarde1@gmail.com>
2021-12-15 22:15:33 +0530

    ADDED: lucky and unlucky days

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -7,21 +7,26 @@ public class GamblerSimulation {
 		int condition = 0;
 		int counterLosePerDay= 0;
 		int counterWonPerDay=0;
+		int [] won = new int[21];
+		int [] lost = new int[21];
 		System.out.println("Gambler starts every day with 100$");
 		int betPlaced = 1;
 		System.out.println("and Playes the bet of " + betPlaced + "$");
 		for(int i=1; i<=20; i++)
 		{
 			int currentAmount = 100;
+			int temp = i;
 			while(currentAmount>=50 && currentAmount<=150) {
 				if(currentAmount==50)
-				{
+				{	
+					won[i] = temp;
 					counterWonPerDay=counterWonPerDay+1;
 					System.out.println("gambler lost & calling a day " + i);
 					break;
 				}
 				else if(currentAmount==150)
 				{	
+					lost[i] = temp;
 					counterLosePerDay=counterLosePerDay+1;
 					System.out.println("gambler won & calling a day " + i);
 					break;
@@ -30,19 +35,20 @@ public class GamblerSimulation {
 				if(condition==1)
 				{
 					currentAmount++;
-				//	System.out.println("Gambler wont the bet and he have " + currentAmount + " $");
+		//			System.out.println("Gambler wont the bet and he have " + currentAmount + " $");
 					
 				}
 				else
 				{
 					currentAmount--;
-				//	System.out.println("Gambler lost the bet and he have " + currentAmount + " $");	
+		//			System.out.println("Gambler lost the bet and he have " + currentAmount + " $");	
 				}
 			}
 		}
 		int totalWonInAmonth = (counterWonPerDay*50) - (counterLosePerDay*50);
 		System.out.println("Total days gambler won = " + counterWonPerDay);
 		System.out.println("Total days gambler lose = " + counterLosePerDay);
+		
 		if(totalWonInAmonth<0)
 		{
 			System.out.println("Total amount lost in 20 days that is in a month is " + Math.abs(totalWonInAmonth));
@@ -57,5 +63,22 @@ public class GamblerSimulation {
 		{
 			System.out.println("Neither won or loss any dollar");
 		}
+		
+			System.out.println("Lucky days are = ");
+			for(int j=1; j<=20; j++) 
+			{
+			if(won[j]!= 0)
+			{
+				System.out.println("lucky day :" + won[j]);
+			}
+			}
+			System.out.println("Unlucky days are");
+			for(int j=1; j<=20; j++) 
+			{
+			if(lost[j] !=0) {
+				System.out.println("unlucky day : " + lost[j]);
+			}	
+			}
+
 	}
 }
