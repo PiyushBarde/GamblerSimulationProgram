cf81f1e56924fd9940464a967b02a99ab778bbeb
PiyushBarde <piyushbarde1@gmail.com>
2021-12-15 20:05:41 +0530

    UPDATED: 50% win lose stake condition

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -4,20 +4,34 @@ public class GamblerSimulation {
 
 	public static void main(String[] args) {
 		int currentAmount = 100;
+		int condition = 0;
 		System.out.println("Gambler starts every day with " + currentAmount + "$");
 		int betPlaced = 1;
 		System.out.println("and Playes the bet of " + betPlaced + "$");
-		int condition = (int) (Math.floor(Math.random()*10)%2);
-		if(condition==1)
-		{
-			currentAmount++;
-			System.out.println("Gambler wont the bet and he have " + currentAmount + " $");
+		while(currentAmount>50 && currentAmount<150) {
 			
-		}
-		else
-		{
-			currentAmount--;
-			System.out.println("Gambler lost the bet and he have " + currentAmount + " $");	
+			if(currentAmount==50)
+			{
+				System.out.println("gambler lost & calling a day");
+				break;
+			}
+			else if(currentAmount==150)
+			{
+				System.out.println("gambler won & calling a day");
+				break;
+			}
+			condition = (int) (Math.floor(Math.random()*10)%2);
+			if(condition==1)
+			{
+				currentAmount++;
+				System.out.println("Gambler wont the bet and he have " + currentAmount + " $");
+				
+			}
+			else
+			{
+				currentAmount--;
+				System.out.println("Gambler lost the bet and he have " + currentAmount + " $");	
+			}
 		}
 	}
 }
